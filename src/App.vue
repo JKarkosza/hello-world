edb66718d442d93ced0e1eb30644e5c451492eec
Jakub <jakub.karkosza@nettmaker.no>
2022-01-26 17:01:28 +0100

    deploy test

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <img alt="Vue logo" src="./assets/gears.svg">
+  <HelloWorld msg="Under construction"/>
 </template>
 
 <script>
@@ -22,5 +22,9 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+img {
+  width: 200px;
 }
 </style>
